473a564f732a5a87fc1d3d3bb6b58dbe22db2cb8
Linas Kapočius <linas.kapocius@gmail.com>
2024-10-05 19:37:16 +0300

    feat: :sparkles: Templates introduced

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import "./styles.css";
 
 const navLinks = [
@@ -11,8 +12,15 @@ const navLinks = [
 
 export default function AuthLayout({ children }: { childer: React.ReactNode }) {
     const pathname = usePathname();
+    const [input, setInput] = useState("");
     return (
         <div>
+            <div>
+                <input
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+            </div>
             {navLinks.map((link) => {
                 const isActive = pathname.startsWith(link.href);
                 return (
