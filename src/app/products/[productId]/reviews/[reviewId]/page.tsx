22c53878519786804cb7bf214b7dfd1235a6851f
Linas Kapočius <linas.kapocius@gmail.com>
2024-10-27 09:57:16 +0200

    fix: :zap: Commented out unused function

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { notFound } from "next/navigation";
 
-function getRandomInt(count: number) {
-    return Math.floor(Math.random() * count);
-}
+// function getRandomInt(count: number) {
+//     return Math.floor(Math.random() * count);
+// }
 
 export default function ReviewDetails({
     params,
