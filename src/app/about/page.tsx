ec2942e3add764b97a21450fe966d1347331ed46
Linas Kapočius <linas.kapocius@gmail.com>
2024-10-27 10:03:21 +0200

    feat: :rocket: Tried to deploy NextJS app of some endpoints as dynamic rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,13 @@
+import { cookies } from "next/headers";
+
 export const metadata = {
     title: "About Codevolution",
 };
 
 export default function About() {
+    const cookieStore = cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
     console.log("About server component");
     return <h1>About me</h1>;
 }
